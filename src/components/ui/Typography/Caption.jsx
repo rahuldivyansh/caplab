824d6e3a330f0da944acc5343feb0dd65b8a3227
thakurthegr8 <rt040371@gmail.com>
2024-02-20 21:46:34 +0530

    added project description markdown

--- src/components/ui/Typography/Caption.jsx
+++ src/components/ui/Typography/Caption.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Caption = (props) => {
-  return <span {...props} />;
+  return <span {...props} className={`caption ${props.className}`} />;
 };
 
 export default Caption;
+
+Caption.defaultProps = {
+  className: "",
+}
