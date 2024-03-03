a5ae2e129fa2aa9960fd5f440d5d87556a8995e4
rahuldivyansh <90146156+rahuldivyansh@users.noreply.github.com>
2024-03-03 23:48:52 +0530

    Added support for docx files using MS Office viewer and for doc using Monaco Editor

--- src/constants/extensions.js
+++ src/constants/extensions.js
@@ -1,22 +1,31 @@
-export const IMAGE_EXTENSIONS = ['jpg', 'jpeg', 'png', 'gif', 'bmp', 'svg', 'webp'];
-export const VIDEO_EXTENSIONS = ['mp4', 'avi', 'mov', 'wmv'];
-export const TEXT_EXTENSIONS = ['txt', 'doc', 'docx', 'pdf'];
-export const PROGRAMMING_EXTENSIONS = [,
+export const IMAGE_EXTENSIONS = [
+  "jpg",
+  "jpeg",
+  "png",
+  "gif",
+  "bmp",
+  "svg",
+  "webp",
+];
+export const VIDEO_EXTENSIONS = ["mp4", "avi", "mov", "wmv"];
+export const TEXT_EXTENSIONS = ["pdf"];
+export const PROGRAMMING_EXTENSIONS = [
+  "doc",
   "txt",
-    "java",
-    "py",
-    "cpp",
-    "c",
-    "h",
-    "cs",
-    "js",
-    "html",
-    "css",
-    "scss",
-    "json",
-    "xml",
-    "yaml",
-    "md",
-    "sh",
-    "sql",
-  ];
+  "java",
+  "py",
+  "cpp",
+  "c",
+  "h",
+  "cs",
+  "js",
+  "html",
+  "css",
+  "scss",
+  "json",
+  "xml",
+  "yaml",
+  "md",
+  "sh",
+  "sql",
+];
