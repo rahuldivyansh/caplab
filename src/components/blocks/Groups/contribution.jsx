4dc3993161944560f0469233d725d579dde79a65
mit-anuj <anujmittal0040@gmail.com>
2024-04-13 15:28:39 +0530

    added the grades functionality

---
+++ src/components/blocks/Groups/contribution.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Contribution = () => {
+  const col = [
+    { key: "name", placeholder: "Name" },
+    { key: 0, placeholder: "TODO" },
+    { key: 1, placeholder: "In Progress" },
+    { key: 3, placeholder: "Completed" },
+  ];
+  return <div>individuals Contribution</div>;
+};
+
+export default Contribution;
